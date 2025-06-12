74a52ece7784c188e00b86af997e2dde0d6e20dc
daguttt <dagutmu667@gmail.com>
2025-06-12 14:26:26 -0500

    fix: handle non-found pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  rescue_from ActiveRecord::RecordNotFound do |_exception|
+    respond_to do |format|
+      format.html { redirect_to root_path, alert: t('.failure') }
+    end
+  end
 end
